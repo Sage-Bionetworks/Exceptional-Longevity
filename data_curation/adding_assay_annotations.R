767460bb3a4e55652edb1d5f7d0574c43a68f5cc
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2024-11-25 10:51:03 -0700

    Update adding_assay_annotations.R

--- data_curation/adding_assay_annotations.R
+++ data_curation/adding_assay_annotations.R
@@ -1,17 +1,16 @@
 ### This script provides an example on how to add assay metadata that are specimen-specific as annotations
 
 # Querying in the file view with the following empty columns: entityID, specimenID, sampleType
-view_id <- "synID" ## Insert file view synID
-view <- synTableQuery(paste0("SELECT * FROM ", view_id))$asDataFrame()
+annotation_query = synTableQuery("select * from synID") # Insert synID of filew view you would like to use to add annotations, query in the file view
+annotation_view = as.data.frame(annotation_query) # Open the file view query as a data frame
 
 # Because the entityID in the metadata is the same as the "id" column in the file view, I will fill the entityID col with the id values
 # This will make the join process easier for the rest of the columns
-view$entityId <- view$id
+annotation_view$entityId <- annotation_view$id
 
 # Now that the entity IDs have been filled out, I want to match the metadata values for those entityIDs to their corresponding columns (specimenID & sampleType) in the file view
 # This will be done via a left join matching on entityID
-
-view <- view %>%
+annotation_view <- annotation_view %>%
   left_join(metadata[, c("entityId", "specimenID", "sampleType")], by = "entityId") %>%        # Performing a left join of the file view with the metadata file
   mutate(
     specimenID = coalesce(specimenID.x, specimenID.y),
@@ -19,9 +18,5 @@ view <- view %>%
   ) %>%
   select(-specimenID.x, -specimenID.y, -sampleType.x, -sampleType.y)
 
-# Now that all of the columns are filled with the metadata I wanted to add, I am going to add the file view metadata back to the data frame for upload
-view$ROW_ID <- substr(view[[1]], 4, 11)
-view$ROW_VERSION <- "1"
-
-# Now uploading back into Synapse
-synStore(Table(view_id, view)) # This upload took about 7 minutes
+# After the columns are filled and the table metadata have been added, upload the table into synapse, adding the annotations to the files in the file view
+synStore(Table("synID", annotation_view))
