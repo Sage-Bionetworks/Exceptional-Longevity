6eae9cec81a037699fa45b7aa374af52ec3d4f70
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2024-11-25 10:48:38 -0700

    Update adding_ELITE_annotations.R

--- data_curation/adding_ELITE_annotations.R
+++ data_curation/adding_ELITE_annotations.R
@@ -1,7 +1,7 @@
 ### This script can be used to add annotations to files that would be the same for every file in a data release (ELITE annotations)
 
 annotation_query = synTableQuery("select * from synID") # Insert synID of filew view you would like to use to add annotations, query in the file view
-annotation_view= as.data.frame(annotation_query) # Open the file view query as a data frame
+annotation_view = as.data.frame(annotation_query) # Open the file view query as a data frame
 
 view$annotation <- "Test Annotation" # Fill in the column(s) of the data frame with the annotations you would like to add to the files in the file view
 view$annotation2 <- "Test Annotation  2"
