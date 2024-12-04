0e054f457983054706180f2e9e74331efdffc4fa
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2024-12-04 13:13:59 -0700

    Update adding_assay_annotations.R
    Added another example to script

--- data_curation/adding_assay_annotations.R
+++ data_curation/adding_assay_annotations.R
@@ -20,3 +20,39 @@ annotation_view <- annotation_view %>%
 
 # After the columns are filled and the table metadata have been added, upload the table into synapse, adding the annotations to the files in the file view
 synStore(Table("synID", annotation_view))
+
+#######################################################################################################
+
+# Here is an example adding more assay annotations at once when the variable type differs between the file view and metadata
+
+# Query in file view
+view_query <- synTableQuery("select * from synID") # Insert file view synID
+view <- as.data.frame(view_query)
+
+# Read in metadata
+assay_metadata <- read.csv(synGet("synID")$path) # Insert assay metadata synID
+
+# This example was for Whole Genome Sequencing assay metadata
+view <- view %>%
+  left_join(
+    assay_metadata[, c(
+      "specimenID", "sequencingBatchID", "aligned_reads", 
+      "alignment_rate", "freemix", "haploid_coverage", 
+      "mean_coverage", "pct_10x", "pct_20x"
+    )],
+    by = "specimenID"
+  ) %>%
+  mutate(
+    sequencingBatchID = coalesce(as.character(sequencingBatchID.x), as.character(sequencingBatchID.y)),
+    aligned_reads = coalesce(as.character(aligned_reads.x), as.character(aligned_reads.y)),
+    alignment_rate = coalesce(as.character(alignment_rate.x), as.character(alignment_rate.y)),
+    freemix = coalesce(as.character(freemix.x), as.character(freemix.y)),
+    haploid_coverage = coalesce(as.character(haploid_coverage.x), as.character(haploid_coverage.y)),
+    mean_coverage = coalesce(as.character(mean_coverage.x), as.character(mean_coverage.y)),
+    pct_10x = coalesce(as.character(pct_10x.x), as.character(pct_10x.y)),
+    pct_20x = coalesce(as.character(pct_20x.x), as.character(pct_20x.y))
+  ) %>%
+  select(-ends_with(".x"), -ends_with(".y"))
+
+# Upload table back into the file view with new, specimen-specific, assay metadata for annotations
+synStore(Table("synID", view)) # Insert file view synID
