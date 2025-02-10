a50da61915fcdc19542fa46f720833668b233476
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2025-02-10 14:05:29 -0700

    Update backend_id_mapping.R
    Updated to proper steps for updating the backend id mapping table

--- cohort_builder/backend_id_mapping.R
+++ cohort_builder/backend_id_mapping.R
@@ -1,28 +1,33 @@
-# Format metadata to the Backend - ID Mapping table format
-metadata <- metadata[, c("individualID.y", "individualID.x", "studyCode.x", "studyCode.y")]
+### Backend ID Mapping table update
+
+### Using Mock Metadata created for the purpose of writing this script, located in Melissa - Sandbox Project on Synapse
+### Pull in Individual Metadata for the study you are adding new individuals for
+metadata <- read.csv(synGet("syn64756927")$path) # Insert the synID of the Individual Metadata you will be using
+
+### Format metadata to the Backend - ID Mapping table format
+metadata$originalId <- metadata$individualId
+
+metadata <- metadata %>%
+  select(individualId, originalId, studyCode, project)
+
 colnames(metadata) <- c("individualId", "originalId", "studyKey", "project")
 
-#Install or load digest package for hashing
+metadata$individualId <- NA
+
+### Install or load digest package for hashing
 library(digest)
 
+### Create a salted hash and use the first 10 alphanumeric characters
 generate_random_id <- function(n, salt) {
-  # Create a salted hash, then take the first 10 alphanumeric characters
   raw_hash <- digest(paste0(salt, Sys.time(), n), algo = "sha256")
   toupper(substr(gsub("[^A-Za-z0-9]", "", raw_hash), 1, 10))
 }
 
-salt <- "stuydkey_salt"  # Insert study key for the stuyd you are adding IDs for
-na_count <- sum(is.na(metadata$individualID)) 
+salt <- "mockstudy_salt"  # Insert study key for the study you are adding IDs for
+na_count <- sum(is.na(metadata$individualId)) 
 
-# Generate random IDs and replace NAs
-updated_metadata$individualID[is.na(metadata$individualID)] <- sapply(1:na_count, generate_random_id, salt)
+### Generate random IDs and replace NAs
+metadata$individualId[is.na(metadata$individualId)] <- sapply(1:na_count, generate_random_id, salt)
 
-# Now, we format this table to match the backend - ID mapping table
-metadata <- metadata[, c("individualID.y", "individualID.x", "studyCode.x", "studyCode.y")]
-colnames(metadata) <- c("individualId", "originalId", "studyKey", "project")
-updated_id_mapping <- metadata
-
-# Time to add the new IDs to the backend table
-
-synStore(Table("synID", metadata)
-
+### Upload new IDs into Backend - ID Mapping table
+synStore(Table("syn64287566", metadata))
