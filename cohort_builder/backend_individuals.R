bb8f5f0e38d85c2325bf4430631092207486bb5e
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2025-02-10 15:02:55 -0700

    Update backend_individuals.R
    Updated instructions on updating backend individuals table

--- cohort_builder/backend_individuals.R
+++ cohort_builder/backend_individuals.R
@@ -1,1 +1,43 @@
-# Add backend individuals instructions here
+### Backend Individuals table update
+
+### Pull in fresh metadata
+metadata <- read.csv(synGet("syn64756955")$path)
+
+metadata <- metadata %>%
+  select(individualId, age, sex, studyCode, diagnosis)
+
+### Bin ages
+metadata$minAge <- metadata$age
+metadata$maxAge <- metadata$age
+
+metadata$age <- as.numeric(metadata$age)
+
+metadata <- metadata %>%
+  mutate(
+    minAge = case_when(
+      age >= 90 & age <= 94 ~ 90,
+      age >= 95 & age <= 99 ~ 95,
+      age >= 100 ~ 100,
+      TRUE ~ age
+    ),
+    maxAge = case_when(
+      age >= 90 & age <= 94 ~ 94,
+      age >= 95 & age <= 99 ~ 99,
+      age >= 100 ~ 100,
+      TRUE ~ age
+    )
+  )
+
+### Format to backend table
+colnames(metadata) <- c("individualId", "age", "Sex", "studyKey", "Diagnosis", "minAge", "maxAge")
+
+metadata <- metadata[, c("studyKey", "individualId", "minAge", "maxAge", "Sex", "Diagnosis")]
+
+### Replace original IDs with Sage IDs, bring in updated ID mapping table
+id_mapping <- as.data.frame(synTableQuery("select * from syn64287566")) ### Filter by study key here as some studies use the same OG IDs
+
+metadata$individualId <- with(id_mapping, 
+                              individualId[match(metadata$individualId, originalId)])
+
+### Upload new Individual data into the Backend - Individuals table
+synStore(Table("syn64290226",metadata))
