c96c276b4de25692ab97907fe25708102d3f071a
Melissa Klein <165932110+melissarklein@users.noreply.github.com>
2025-06-17 09:03:08 -0600

    Update metadata_annotations.R

--- Interoperability/BioProject_Data_Index/metadata_annotations.R
+++ Interoperability/BioProject_Data_Index/metadata_annotations.R
@@ -1,5 +1,11 @@
 ### Gorbunova file annotations - Example
 
+### After the link entities have been created using the previous script, the naming format looks something like this:
+### "GSM5730459 AMS1Lung Acomys russatus RNASeq"
+### This format is important for the below code to work, but may not be exactly the same for each BioProject study.
+
+# Create an entity view including all of the link entities that were creating using the previous script.
+
 # Get the entity view to add annotations
 entity_view <- synTableQuery("SELECT * FROM syn66351296")
 df <- as.data.frame(entity_view)
